cf484f45c2a8c8730e9783e1b64527ecd1748083
will <williamgeorge.barker@gmail.com>
2022-10-24 19:32:58 -0400

    fix dbt deps command

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-dbt deps .  # Pulls the most recent version of the dependencies listed in your packages.yml from git
-dbt debug --target prod
+dbt deps  # Pulls the most recent version of the dependencies listed in your packages.yml from git
 dbt run --target prod
 dbt docs generate

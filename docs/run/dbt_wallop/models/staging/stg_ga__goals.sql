1403cdcc48d1700bcb483c09ccc671b130ea3d6d
will <williamgeorge.barker@gmail.com>
2022-10-24 20:37:18 -0400

    docs generate

---
+++ docs/run/dbt_wallop/models/staging/stg_ga__goals.sql
@@ -0,0 +1,44 @@
+
+
+  create or replace view `dbt-wallop-dev-1`.`staging`.`stg_ga__goals`
+  OPTIONS()
+  as WITH
+ga_goals AS (
+    SELECT
+        g1.adwordsAdGroupID AS adwords_adgroup_id,
+        g1.campaign,
+        g1.channelGrouping AS channel_grouping,
+        g1.source,
+        g1.sourceMedium AS source_medium,
+        g1.goal1Completions AS goal1_completions,
+        g1.goal2Completions AS goal2_completions,
+        g1.goal3Completions AS goal3_completions,
+        g1.goal4Completions AS goal4_completions,
+        g1.goal5Completions AS goal5_completions,
+        g1.goal6Completions AS goal6_completions,
+        g1.goal7Completions AS goal7_completions,
+        g1.goal8Completions AS goal8_completions,
+        g1.goal9Completions AS goal9_completions,
+        g1.goal10Completions AS goal10_completions,
+        g2.goal11Completions AS goal11_completions,
+        g2.goal12Completions AS goal12_completions,
+        g2.goal13Completions AS goal13_completions,
+        g2.goal14Completions AS goal14_completions,
+        g2.goal15Completions AS goal15_completions,
+        g2.goal16Completions AS goal16_completions,
+        g2.goal17Completions AS goal17_completions,
+        g2.goal18Completions AS goal18_completions,
+        g2.goal19Completions AS goal19_completions,
+        g2.goal20Completions AS goal20_completions,
+        g1.date,
+        g1.configName AS config_name,
+    FROM `bigquery-312020`.`analytics_wallop`.`pma_Google_Analytics_UA_Goals_1_10_*` g1
+    JOIN `bigquery-312020`.`analytics_wallop`.`pma_Google_Analytics_UA_Goals_11_20_*` g2
+        ON g1.adwordsAdGroupID = g2.adwordsAdGroupID
+        AND g1.campaign = g2.campaign
+        AND g1.source = g2.source
+        AND g1.date = g2.date
+        )
+
+SELECT * FROM ga_goals;
+

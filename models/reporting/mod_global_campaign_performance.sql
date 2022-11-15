cf14317fc7653ee0a17ba3a1ad52a9504b0cb9d1
will <williamgeorge.barker@gmail.com>
2022-11-14 22:52:56 -0500

    created mod_global_campaign_performance

---
+++ models/reporting/mod_global_campaign_performance.sql
@@ -0,0 +1,74 @@
+WITH
+gads_core AS (
+    SELECT
+        'Google Ads' AS data_source,
+        account_id,
+        account_name,
+        campaign_id,
+        campaign_name,
+        adgroup_id,
+        adgroup_name,
+        CAST(NULL AS STRING) AS ad_id,
+        CAST(NULL AS STRING) AS ad_name,
+        headline,
+        description,
+        cost,
+        impressions,
+        clicks,
+        date,
+    FROM {{ref('stg_gads__ad_performance')}}
+    ),
+
+fb_core AS (
+    SELECT
+        'Facebook' AS data_source,
+        account_id,
+        account_name,
+        campaign_id,
+        campaign_name,
+        adgroup_id,
+        adgroup_name,
+        ad_id,
+        ad_name,
+        headline,
+        description,
+        cost,
+        impressions,
+        clicks,
+        date,
+    FROM {{ref('stg_fb__ad_performance')}}
+    ),
+
+bing_core AS (
+    SELECT
+        'Bing' AS data_source,
+        account_id,
+        account_name,
+        campaign_id,
+        campaign_name,
+        CAST(NULL AS STRING) AS adgroup_id,
+        CAST(NULL AS STRING) AS adgroup_name,
+        CAST(NULL AS STRING) AS ad_id,
+        CAST(NULL AS STRING) AS ad_name,
+        CAST(NULL AS STRING) AS headline,
+        description,
+        cost,
+        impressions,
+        clicks,
+        date,
+    FROM {{ref('stg_bing__ad_performance')}}
+    ),
+
+unioned AS (
+    SELECT * FROM gads_core
+
+    UNION ALL
+
+    SELECT * FROM fb_core
+
+    UNION ALL
+
+    SELECT * FROM bing_core
+    )
+
+SELECT * FROM unioned

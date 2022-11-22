c2df256ac7a7d4a9447a158de20664f682d2d7e9
will <williamgeorge.barker@gmail.com>
2022-11-21 21:21:15 -0500

    add in account_name from google_analytics

--- models/reporting/mod_global_campaign_performance.sql
+++ models/reporting/mod_global_campaign_performance.sql
@@ -34,6 +34,7 @@ campaign_rollup AS (
 ga_campaign_rollup AS (
     SELECT
         'Google Analytics' AS data_source,
+        ga_account AS account_name,
         channel_grouping,
         utm_campaign,
         utm_source,
@@ -70,14 +71,14 @@ ga_campaign_rollup AS (
         ) ga
     FROM {{ref('int_ga__performance_final')}}
     GROUP BY
-        1, 2, 3, 4, 5, 6
+        1, 2, 3, 4, 5, 6, 7
         ),
 
 final AS (
     SELECT
         COALESCE(cr.data_source, gcr.data_source) AS data_source,
         cr.account_id,
-        cr.account_name,
+        COALESCE(cr.account_name, gcr.account_name) AS account_name,
         cr.campaign_id,
         cr.campaign_name,
         COALESCE(cr.utm_source, gcr.utm_source) AS utm_source,

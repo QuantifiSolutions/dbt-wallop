f1d62ee9020d57a13b49f925c4c46eddd0939bad
will <williamgeorge.barker@gmail.com>
2022-12-02 21:54:51 -0800

    solve duplication by adding campaign_name into client_key

--- docs/run/dbt_wallop/models/reporting/rep_global_campaign_performance_tbl.sql
+++ docs/run/dbt_wallop/models/reporting/rep_global_campaign_performance_tbl.sql
@@ -17,7 +17,11 @@ final AS (
         cmp.*
     FROM `dbt-wallop-dev-1`.`reporting`.`mod_global_campaign_performance` cmp
     LEFT JOIN `dbt-wallop-dev-1`.`reporting`.`mod_composite_client_key` cli
-        ON CONCAT(cmp.account_id, IFNULL(cmp.campaign_id, '')) = cli.client_key
+        ON CONCAT(
+                cmp.account_id,
+                IFNULL(cmp.campaign_id, ''),
+                IFNULL(cmp.campaign_name, '')
+                ) = cli.client_key
         )
 
 SELECT * FROM final
--- models/reporting/rep_global_ad_performance_tbl.sql
+++ models/reporting/rep_global_ad_performance_tbl.sql
@@ -10,7 +10,11 @@ final AS (
         ad.*
     FROM {{ref('mod_global_ad_performance')}} ad
     LEFT JOIN {{ref('mod_composite_client_key')}} cli
-        ON CONCAT(ad.account_id, IFNULL(ad.campaign_id, '')) = cli.client_key
+        ON CONCAT(
+                ad.account_id,
+                IFNULL(ad.campaign_id, ''),
+                IFNULL(ad.campaign_name, '')
+                ) = cli.client_key
         )
 
 SELECT * FROM final

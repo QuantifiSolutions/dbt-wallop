70d2327a70208541895a48f7f910ea19371680ac
will <williamgeorge.barker@gmail.com>
2022-12-05 21:37:26 -0800

    adjust exclusions logic for zingerman, ranch

--- docs/compiled/dbt_wallop/models/reporting/rep_global_ad_performance_tbl.sql
+++ docs/compiled/dbt_wallop/models/reporting/rep_global_ad_performance_tbl.sql
@@ -13,8 +13,15 @@ final AS (
                 IFNULL(ad.campaign_id, ''),
                 IFNULL(ad.campaign_name, '')
                 ) = cli.client_key
+        ),
+
+final_w_exclusions AS (
+    SELECT *
+    FROM final
+    WHERE
+        NOT (campaign_name IS NOT NULL AND REGEXP_CONTAINS(campaign_name, '(?i)(broad match)'))
+        AND NOT (client_name = 'Zingerman' AND campaign_name = 'Bid Strategy')
+        AND NOT (client_name = 'The Ranch At Rock Creek' AND campaign_name = 'TRRC - Search - US & CA No extensions')
         )
 
-SELECT * FROM final
-WHERE
-    NOT REGEXP_CONTAINS(campaign_name, '(?i)(broad match)')
+SELECT * FROM final_w_exclusions

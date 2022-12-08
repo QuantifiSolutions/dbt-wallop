69a00bad522f6848ef8a996f7c67461aa2f49db7
will <williamgeorge.barker@gmail.com>
2022-12-08 08:57:42 -0500

    fairmont mapping; mirror lake exclusions

--- docs/compiled/dbt_wallop/models/google_analytics/stg_ga__goals.sql
+++ docs/compiled/dbt_wallop/models/google_analytics/stg_ga__goals.sql
@@ -1,6 +1,18 @@
 WITH
+-- union google analytics main and separate client sources
+processing_layer_g1 AS (
+    SELECT * FROM `bigquery-312020`.`analytics_wallop`.`pma_Google_Analytics_UA_Goals_1_10_*`
+    UNION ALL
+    SELECT * FROM `bigquery-312020`.`analytics_wallop`.`pma_Google_Analytics_UA_Goals_1_10_Fairmont_*`
+    ),
+
+processing_layer_g2 AS (
+    SELECT * FROM `bigquery-312020`.`analytics_wallop`.`pma_Google_Analytics_UA_Goals_11_20_*`
+    ),
+
+-- Select distinct in case there exists overlap between google analytics main and seperate client sources
 ga_goals AS (
-    SELECT
+    SELECT DISTINCT
         NULLIF(COALESCE(g1.adwordsAdGroupID, g2.adwordsAdGroupID), '(not set)') AS adwords_adgroup_id,
         NULLIF(COALESCE(g1.campaign, g2.campaign), '(not set)') AS campaign,
         COALESCE(g1.channelGrouping, g2.channelGrouping) AS channel_grouping,
@@ -32,8 +44,8 @@ ga_goals AS (
         REGEXP_EXTRACT(COALESCE(g1.configName, g2.configName), "(?i)^.* -> ([a-z0-9\\(\\)\\-\\./': é&]+) -> .*$") AS ga_property,
         REGEXP_EXTRACT(COALESCE(g1.configName, g2.configName), "(?i)^.* -> .* -> ([a-z0-9\\(\\)\\-\\./': é&]+) \\([0-9]+\\)$") AS ga_view_name,
         REGEXP_EXTRACT(COALESCE(g1.configName, g2.configName), ".* -> .* -> .* \\(([0-9]+)\\)$") AS ga_view_number
-    FROM `bigquery-312020`.`analytics_wallop`.`pma_Google_Analytics_UA_Goals_1_10_*` g1
-    FULL OUTER JOIN `bigquery-312020`.`analytics_wallop`.`pma_Google_Analytics_UA_Goals_11_20_*` g2
+    FROM processing_layer_g1 g1
+    FULL OUTER JOIN processing_layer_g2 g2
         ON g1.adwordsAdGroupID = g2.adwordsAdGroupID
         AND g1.campaign = g2.campaign
         AND g1.source = g2.source
